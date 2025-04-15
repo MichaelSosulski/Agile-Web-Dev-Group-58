2e46a92bea5cb0a3725275d2427de1de184d2138
MichaelSosulski <23385553@student.uwa.edu.au>
2025-04-15 23:04:17 +0800

    Changed toggleVisible function to hide other dropdowns that are not the target.

--- navbar.js
+++ navbar.js
@@ -1,5 +1,12 @@
 function toggleVisible(dropdownId) {
-    document.getElementById(dropdownId).classList.toggle("show");
+    const dropdown = document.getElementById(dropdownId);
+
+    document.querySelectorAll('.dropdown-content').forEach(container => {
+        if (container.id !== dropdownId) {
+            container.style.display = 'none';
+        }
+    });
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
 document.getElementById("loginBtn").addEventListener("click", function() {toggleVisible("loginDropdown"); });
 document.getElementById("signupBtn").addEventListener("click", function() {toggleVisible("signupDropdown"); });
