87349dff3b0503c1b51ec525036497f85008370c
TysonHaines <168697890+TysonHaines@users.noreply.github.com>
2025-04-20 11:22:54 +0800

    Update navbar.js to include form function
    drop down function hides forms until login or sign in buttons from the drop down menu are clicked

--- navbar.js
+++ navbar.js
@@ -2,3 +2,17 @@ function toggleVisible() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 document.getElementById("loginBtn").addEventListener("click", toggleVisible);
+
+function dropForm(formId) {
+    const dropdown = document.getElementById(formId);
+
+    
+    document.querySelectorAll('.form-container').forEach(container => {
+        if (container.id !== formId) {
+            container.style.display = 'none';
+        }
+    });
+
+    // Toggle the selected dropdown
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
