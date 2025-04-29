30bc99641eb99bcc3635517a5dff37129c0bc95f
MichaelSosulski <23385553@student.uwa.edu.au>
2025-04-30 01:50:24 +0800

    Functions in navbar.js now have comments for clarity of their purpose.

--- navbar.js
+++ navbar.js
@@ -9,7 +9,9 @@ function toggleVisible(dropdownId) {
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
 
+//When object in window is clicked, if object isn't part of a dropdown, hide dropdowns
 window.addEventListener("click", (e) => {
+    //Cancel handler if clicked inside of dropdown
     let parent = e.target.parentNode;
     while (parent.nodeName !== "HTML") {
         if (parent.className.split(' ').includes("dropdown")) {
@@ -17,8 +19,9 @@ window.addEventListener("click", (e) => {
         }
         parent = parent.parentNode;
     }
-    
+    //Checks if element pressed is not a child of an element of the dropdown class.
     if (!e.target.parentNode.matches('.dropdown')) {
+        //hide .dropdown-contents
         let dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
         for (i = 0; i < dropdowns.length; i++) {
