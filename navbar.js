51a9b032c58f77f361e6e7111aacbc1b2439c01d
MichaelSosulski <23385553@student.uwa.edu.au>
2025-04-30 01:42:08 +0800

    Added window click event handler to close all dropdowns when user clicks outside a .dropdown element

--- navbar.js
+++ navbar.js
@@ -8,5 +8,27 @@ function toggleVisible(dropdownId) {
     });
     dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
+
+window.addEventListener("click", (e) => {
+    let parent = e.target.parentNode;
+    while (parent.nodeName !== "HTML") {
+        if (parent.className.split(' ').includes("dropdown")) {
+            return;
+        }
+        parent = parent.parentNode;
+    }
+    
+    if (!e.target.parentNode.matches('.dropdown')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+          let openDropdown = dropdowns[i];
+          if (openDropdown.style.display === "block") {
+            openDropdown.style.display = "none";
+          }
+        }
+      }
+})
+
 document.getElementById("loginBtn").addEventListener("click", function() {toggleVisible("loginDropdown"); });
 document.getElementById("signupBtn").addEventListener("click", function() {toggleVisible("signupDropdown"); });
