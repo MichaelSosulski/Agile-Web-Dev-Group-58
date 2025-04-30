8f803f1c7a05a84fc83f2c65499e4f46ccbed3f8
Thyler Cung <thyler.cung@gmail.com>
2025-04-30 09:30:59 +0800

    Changed Nav Bar Design

--- navbar.js
+++ navbar.js
@@ -1,12 +1,13 @@
 function toggleVisible(dropdownId) {
-    const dropdown = document.getElementById(dropdownId);
+const dropdown = document.getElementById(dropdownId);
 
-    document.querySelectorAll('.dropdown-content').forEach(container => {
-        if (container.id !== dropdownId) {
-            container.style.display = 'none';
-        }
-    });
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+document.querySelectorAll('.dropdown-content').forEach(container => {
+    if (container.id !== dropdownId) {
+        container.style.display = 'none';
+    }
+});
+dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
 }
 document.getElementById("loginBtn").addEventListener("click", function() {toggleVisible("loginDropdown"); });
 document.getElementById("signupBtn").addEventListener("click", function() {toggleVisible("signupDropdown"); });
+document.getElementById("profileBtn").addEventListener("click", function() {toggleVisible("profileDropdown"); });
