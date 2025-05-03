d7a9786600f7b8cd5af662a1e074f456f50b4a26
TysonHaines <168697890+TysonHaines@users.noreply.github.com>
2025-05-03 19:31:10 +0800

    server
    Setup initial server:
    includes:
    -modification of all links using 'url_for' method in routes.py
    -server structure: static file, templates file, __init__.py file, and more.
    
    note: 
    -this was quite a significant commit so look out for any errors

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask,  render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/Profile')
+def profile():
+    return render_template('ProfilePgae.html')
+
+@app.route('/Collection')
+def collection():
+    return render_template('CollectionPage.html')
+
+@app.route('/Friends')
+def friends():
+    return render_template('FriendsPage.html')
+
+@app.route('/Stats')
+def stats():
+    return render_template('StatsPage.html')
+
+@app.route('/Welcome')
+def welcome():
+    return render_template('WelcomePage.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
+from app import routes
