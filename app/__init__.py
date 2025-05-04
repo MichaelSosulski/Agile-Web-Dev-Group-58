dd8d67705a2423d5b80eeabf3f19d3e361d4dcdc
Thyler Cung <thyler.cung@gmail.com>
2025-05-04 18:46:09 +0800

    Linked to database with SQL-Alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask,  render_template
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 
-app = Flask(__name__)
+application = Flask(__name__)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
-if __name__ == '__main__':
-    app.run()
-
-from app import routes
+from app import routes, models
