5ac24a0eddfd57f3bd02874af0bffa97e8527588
MichaelSosulski <23385553@student.uwa.edu.au>
2025-05-05 19:10:31 +0800

    Fixed a bug where the welcome page doesn't have a navbar block that applies to the setActiveNavbar function in navbar.js

--- app/static/js/navbar.js
+++ app/static/js/navbar.js
@@ -1,8 +1,11 @@
 /*Set the class of a navbar list item to "active" if the list item id is equal to
  the second word in <title> tag*/
  function setActiveNavbar () {
-    let pageName = document.title.split(' ')[1];
-    document.getElementById(pageName).className = "active";
+    const pageName = document.title.split(' ')[1];
+    const navbarItem = document.getElementById(pageName);
+    if (navbarItem !== null) {
+    navbarItem.className = "active";
+    }
  }
 setActiveNavbar();
 
