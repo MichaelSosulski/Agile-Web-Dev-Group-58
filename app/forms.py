f939da57185f1b18a4159b003d24296a077ad060
MichaelSosulski <23385553@student.uwa.edu.au>
2025-05-05 21:47:06 +0800

    Added a validator to the signup form that checks confirmPassword is equal to password.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, EmailField, PasswordField, IntegerField,TextAreaField, RadioField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
     username = StringField("Username:", validators=[DataRequired()])
@@ -11,5 +11,5 @@ class SignupForm(FlaskForm):
     username = StringField("Username:", validators=[DataRequired()])
     email = EmailField("Email:", validators=[DataRequired()])
     password = PasswordField("Password:", validators=[DataRequired()])
-    confirmPassword = PasswordField("confirm password:", validators=[DataRequired()])
+    confirmPassword = PasswordField("confirm password:", validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit_signup = SubmitField("Sign up")
