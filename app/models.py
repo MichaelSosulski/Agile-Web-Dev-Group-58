860b1d4e97add01e3d583f56ab8de07cf91d95c4
MichaelSosulski <23385553@student.uwa.edu.au>
2025-05-11 21:10:53 +0800

    Database models rewritten to support adding external film data, movie name migrated to be called movie title to match variable storing title.

--- app/models.py
+++ app/models.py
@@ -1,48 +1,60 @@
 from app import db
 
-#For movies we currently have name, runtime and genre lmk if we need more data
-class Movies(db.Model):
+#Movie data
+class Movie(db.Model):
     movie_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     release_year = db.Column(db.Integer)
-    watch_date = db.Column(db.Date)
-    rating = db.Column(db.Integer)
-    review = db.Column(db.String(300))
-    genre = db.Column(db.String(100), nullable=True) 
-    collections = db.relationship('Collections', back_populates='movie')  
+    director = db.Column(db.String(50), nullable=False)
+    run_time = db.Column(db.Integer)
+    plot = db.Column(db.String(300), nullable=False)
+    poster = db.Column(db.String(200), nullable=False)
+
+    movie_genres = db.relationship('MovieGenre', back_populates='movie')
+    collection = db.relationship('Collection', back_populates='movie')  
+
+#A movie can have multiple genres
+class MovieGenre(db.Model):
+    movie_genre_id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.movie_id'), nullable=False)
+    genre = db.Column(db.String(20), nullable=False)
+
+    movie = db.relationship('Movie', back_populates='movie_genres')
 
 #Users we just have username, email and password no salting just yet
-class Users(db.Model):
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    collections = db.relationship('Collections', back_populates='user')
+    collection = db.relationship('Collection', back_populates='user')
     
 #User and movie are attibutes of a collection as a single movie can be in multiple collections
-class Collections(db.Model):
+class Collection(db.Model):
     collection_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
-    collection_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.movie_id'), nullable=False)
+    watch_date = db.Column(db.Date, nullable=True)
+    rating = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(300), nullable=True)
     category = db.Column(db.String(50)) 
     
-    user = db.relationship('Users', back_populates='collections')
-    movie = db.relationship('Movies', back_populates='collections')
+    user = db.relationship('User', back_populates='collection')
+    movie = db.relationship('Movie', back_populates='collection')
 
 #I think we should store firends as both way like A is a friend of B and B is also a friend of A
-class Friends(db.Model):
+class Friend(db.Model):
     friendship_id = db.Column(db.Integer, primary_key=True)
-    friend_a_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    friend_b_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    friend_a = db.relationship('Users', foreign_keys=[friend_a_id])
-    friend_b = db.relationship('Users', foreign_keys=[friend_b_id])
+    friend_a_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    friend_b_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    friend_a = db.relationship('User', foreign_keys=[friend_a_id])
+    friend_b = db.relationship('User', foreign_keys=[friend_b_id])
     
-class Posts(db.Model):
+class Post(db.Model):
     post_id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     movie_name = db.Column(db.String(100), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    sender = db.relationship('Users', foreign_keys=[sender_id])
-    receiver = db.relationship('Users', foreign_keys=[receiver_id])
+    sender = db.relationship('User', foreign_keys=[sender_id])
+    receiver = db.relationship('User', foreign_keys=[receiver_id])
