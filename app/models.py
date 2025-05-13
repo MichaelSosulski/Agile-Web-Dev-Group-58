37d33c0b87bee79dad21cca1f26fa471eca3ec3c
Nico Buchanan <nico.s.buchanan@gmail.com>
2025-05-13 17:45:19 +0800

    Update User model to use SQAlchemy mapped syntax so fields are assigned a type using Python type hints

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
 from app import db
 
 #Movie data
@@ -23,10 +26,10 @@ class MovieGenre(db.Model):
 
 #Users we just have username, email and password no salting just yet
 class User(db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    user_id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     collection = db.relationship('Collection', back_populates='user')
     
 #User and movie are attibutes of a collection as a single movie can be in multiple collections
