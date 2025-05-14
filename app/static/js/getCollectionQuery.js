6d498af3ec904359ec5fb95029537d0d8bc81538
MichaelSosulski <23385553@student.uwa.edu.au>
2025-05-15 04:54:49 +0800

    Added client side rendering of film data searched from user's collections.

---
+++ app/static/js/getCollectionQuery.js
@@ -0,0 +1,85 @@
+const serverDomain = "http://127.0.0.1:5000";
+const serverRoute = "/get_film/";
+const searchBox = document.getElementById("searchFilm");
+const filmBox = document.getElementById("filmData");
+
+
+let searchTerm = '';
+let timeoutId;
+
+function clearChildren(parent) {
+        parent.innerHTML = "";
+}
+
+function getUserFilmInfo(search) {
+    if (!search) {
+        clearChildren(filmBox);
+        return;
+    }
+    const url = `${serverDomain}${serverRoute}${encodeURIComponent(search)}`;
+
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            clearChildren(filmBox);
+
+            if (data && data.length > 0) {
+                data.forEach(film => {
+                    console.log(film);
+                    //Populate posterDiv
+                    const posterDiv = document.createElement("div");
+                    posterDiv.classList.add("col-md-2");
+                    const posterImg = document.createElement("img");
+                    posterImg.src = film.poster;
+                    posterImg.alt = "poster of " + film.title;
+                    posterDiv.appendChild(posterImg);
+                    filmBox.appendChild(posterDiv);
+                    //Populate filmDiv
+                    const filmDiv = document.createElement("div");
+                    filmDiv.classList.add("col-md-5");
+                    filmDiv.innerHTML = `
+                        <h3>${film.title} (${film.release_year})</h3>
+                        <p>Director: ${film.director}</p>
+                        <p>Genres: ${film.genres}</p>
+                        <p>Run Time: ${parseInt (film.run_time/60)}hr ${parseInt (film.run_time%60)} min</p>
+                        <p>${film.plot}</p>
+                    `;
+                    filmBox.appendChild(filmDiv);
+                    //Populate userDiv
+                    const userDiv = document.createElement("div");
+                    userDiv.classList.add("col-md-5")
+                    userDiv.innerHTML = `<br>
+                        <p>Watch Date: ${film.watch_date}</p>
+                        <p>Rating: ${film.rating}/5</p>
+                        <p>Review: ${film.review}</p>
+                    `;
+                    filmBox.appendChild(userDiv);
+
+                });
+            } else {
+                filmBox.innerHTML += `<p>No films found matching "${search}".</p>`;
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.error("Error: ${textStatus}, ${errorThrown}");
+            clearChildren(filmBox);
+        }
+    });
+}
+
+function handleSearchChange(event) {
+    searchTerm = event.target.value;
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+        if (searchTerm) {
+            getUserFilmInfo(searchTerm);
+        } else {
+            clearChildren(filmBox);
+        }
+    }, 300);
+}
+
+searchBox.addEventListener("input", handleSearchChange);
